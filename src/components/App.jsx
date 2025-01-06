52523132fc2778d8ab1e5e1c6d906022eaf41503
aminetiouk <tiouk.amine@gmail.com>
2025-01-06 21:53:34 +0100

    Initial commit: Set up project structure

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from "./Header"
+import { Container } from "./Container"
+import { Footer } from "./Footer"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Container />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
