b208044851a794f7e0a0419ed3e73fa923350fc2
aminetiouk <tiouk.amine@gmail.com>
2025-01-07 22:29:06 +0100

    feat: add Textarea and Stats

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,30 @@
-import { Header } from "./Header"
-import { Container } from "./Container"
-import { Footer } from "./Footer"
+import Header  from "./Header"
+import Footer from "./Footer"
+import Main from "./Main"
+import Textarea from "./Textarea"
+import Stats from "./Stats"
+import { useState } from "react"
 
 function App() {
+  const [stats, setStats] = useState({
+    numberOfWords: 0,
+    numberOfCharacters: 0,
+    instagramCharactersLeft: 2200,
+    facebookCharactersLeft: 6320,
+  });
+
   return (
     <>
       <Header />
-      <main>
-        <Container />
-      </main>
+
+      <Main>
+        <Textarea setStats={setStats} />
+        <Stats stats={stats}/>
+      </Main>
+
       <Footer />
     </>
-  )
+  );
 }
 
 export default App
