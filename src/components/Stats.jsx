611eb14f8c3afceea6ccd72fa9d77e9f012acfa6
aminetiouk <tiouk.amine@gmail.com>
2025-01-07 22:33:08 +0100

    feat: add Stats component

---
+++ src/components/Stats.jsx
@@ -0,0 +1,21 @@
+import H2 from './H2'
+
+export default function Stats({ stats }) {
+  return (
+    <section className="stats">
+      <Stat number={stats.numberOfWords} type="words" />
+      <Stat number={stats.numberOfCharacters} type="characters" />
+      <Stat number={stats.instagramCharactersLeft} type="Instagram" />
+      <Stat number={stats.facebookCharactersLeft} type="Facebook" />
+    </section>
+  )
+}
+
+function Stat({ number, type }) {
+  return (
+    <section className="stat">
+      <span className="stat__number">{number}</span>
+      <H2>{type}</H2>
+    </section>
+  )
+}
