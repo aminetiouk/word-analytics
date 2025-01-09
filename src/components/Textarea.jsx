78c511b6400a52c37f492c48f77382c489186cb8
aminetiouk <tiouk.amine@gmail.com>
2025-01-09 23:34:19 +0100

    feat: implement logic word analytics

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,9 +1,27 @@
-export default function Textarea() {
+import { useState } from 'react';
+
+export default function Textarea({ setStats }) {
+  const [text, setText] = useState("");
+
+
+  const handleChange = (event) => {
+
+    let text = event.target.value;
+    setText(text);
+    setStats({
+      numberOfWords: text.split(" ")[0] === "" ? 0 : text.split(" ").length,
+      numberOfCharacters: text.length,
+      instagramCharactersLeft: 2200 - text.length,
+      facebookCharactersLeft: 6320 - text.length,
+    });
+  };
   return (
     <section className="textarea">
       <textarea
         spellCheck="false"
         placeholder="Enter or paste your text here"
+        onChange={handleChange}
+        value={text}
       ></textarea>
     </section>
   );
